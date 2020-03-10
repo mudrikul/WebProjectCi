bfd5715dfa4c68e536473eaf089eec05f90a35d6
mudrikul <kyutkitten@gmail.com>
2020-03-10 13:39:58 +0700

    set rules

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,16 +6,40 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Auth extends CI_Controller
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+
     public function index()
     {
-        $data = array('content' => 'auth/login');
+        $data = array('content' => 'auth/login', 'title' => 'login page');
         $this->load->view('auth/template', $data);
     }
 
     public function registration()
     {
-        $data = array('content' => 'auth/registration');
-        $this->load->view('auth/template', $data);
+        $config = array(
+            array(
+                'field' => 'name',
+                'label' => 'Name',
+                'rules' => 'required|trim'
+            ),
+            array(
+                'field' => 'email',
+                'label' => 'Email',
+                'rules' => 'required|trim|valid_email'
+            )
+        );
+        $this->form_validation->set_rules($config);
+        if ($this->form_validation->run() == false) {
+            $data = array('content' => 'auth/registration', 'title' => 'Register page');
+            $this->load->view('auth/template', $data);
+        } else {
+            echo "data done";
+        }
     }
 
     public function hal()
